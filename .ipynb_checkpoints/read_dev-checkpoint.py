694d0822dddea8233f726f3e63ca40862454ecb8
Karanraj Chauhan <chauhank@bu.edu>
2019-09-21 14:28:36 -0400

    Initial experiment w/ itertools - remove checkpoint and vscode tracking

--- .ipynb_checkpoints/read_dev-checkpoint.py
+++
@@ -1,8 +0,0 @@
-import torch
-from petastorm import make_reader, make_batch_reader
-from petastorm.pytorch import DataLoader
-
-DATA_DIR = 'file:///home/kachauha/Downloads/data_Q4_2018_parquet/part.0.parquet'
-with DataLoader(make_batch_reader(DATA_DIR, num_epochs=10), batch_size=64) as train_loader:
-    for batch in train_loader:
-        print(batch)
