f6eb892c25e90bcf58b082ad38c2278d0eb8f1c9
Karanraj Chauhan <chauhank@bu.edu>
2019-10-24 15:29:21 -0400

    Updated model arch. 2x128h lstm + 3x256h lstm

--- models.py
+++ models.py
@@ -5,14 +5,17 @@ import torch.nn as nn
 class LSTMtoy(nn.Module):
     def __init__(self, input_dim, output_dim):
         super(LSTMtoy, self).__init__()
-        self.lstm = nn.LSTM(input_dim, 256, 5)
+        self.lstm1 = nn.LSTM(input_dim, 128, 2)
+        self.lstm2 = nn.LSTM(128, 256, 3)
         # self.lstm1 = nn.LSTMCell(1, 51)
         # self.lstm2 = nn.LSTMCell(51, 51)
         self.linear = nn.Linear(256, output_dim)
 
     def forward(self, x):
-        output, hidden = self.lstm(x)
+        output, hidden = self.lstm1(x)
+        output, hidden = self.lstm2(output)
         # take output of last lstm cell for the last time cel
         # and feed to linear layer
         logits = self.linear(output[-1, :])
         return logits
+
