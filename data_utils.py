dd9735986bae539fbb100774205cb37a45db88d3
Karanraj Chauhan <chauhank@bu.edu>
2019-10-28 12:29:05 -0400

    Updated data saving ipynb. Added code to convert csv to pt for failed and working (not yet for meta)

--- data_utils.py
+++ data_utils.py
@@ -1,4 +1,5 @@
 import os
+from glob import glob as gg
 from os.path import join as ospj
 from itertools import chain, cycle
 
@@ -6,16 +7,17 @@ import torch
 import pandas as pd
 from sklearn.model_selection import  train_test_split
 
+from joblib import Parallel, delayed, parallel_backend
 
+
+# # get metadata for scaling - moved this outside fn so that it's not read again and again
+# means = pd.read_csv(ospj(META_DIR, 'means.csv'), header=None).set_index(0).transpose()
+# stds = pd.read_csv(ospj(META_DIR, 'stds.csv'), header=None).set_index(0).transpose()
 # def bb_data_transform(df):
+#     global means, stds
 #     # scale from bytes to gigabyte
 #     # FIXME: this is done coz mean centering does not work w/ large numbers
 #     df['capacity_bytes'] /= 10**9
-
-#     # get metadata for scaling
-#     meta_dir = '/home/kachauha/Downloads/data_Q4_2018_serials/meta'
-#     means = pd.read_csv(os.path.join(meta_dir, 'means.csv'), header=None).set_index(0).transpose()
-#     stds = pd.read_csv(os.path.join(meta_dir, 'stds.csv'), header=None).set_index(0).transpose()
 
 #     # 0 mean, 1 std
 #     # FIXME: subtract and divide w/out using index else nans
@@ -28,6 +30,7 @@ from sklearn.model_selection import  train_test_split
 #     df = df / stds.values
 
 #     # to tensor
+#     # NOTE: need to make (seq_len, batch, input_size) from (batch, seq_len, input_size)
 #     return torch.Tensor(df.values)
 
 
@@ -119,19 +122,75 @@ class BackblazeSingleDriveDataset(torch.utils.data.IterableDataset):
         return len(self._df) - self.time_window_size + 1
 
 
+class BackblazeSingleDrivePtDataset(torch.utils.data.IterableDataset):
+    """
+    Dataset to iterate over data from a serial number, in chunks of time_window days
+
+    This is an IterableDataset because that makes it easier for chaining.
+    If it were a regular Dataset, then random reads would be possible which means
+    each csv file could be read multiple times which can be expensive.
+    """
+    def __init__(self, fpath, feat_cols=None, target_cols=['status'], time_window_size=6, transform=None, target_transform=None):
+        super(BackblazeSingleDrivePtDataset, self).__init__()
+
+        # ensure data file exists
+        assert os.path.exists(fpath)
+
+        # time frame of data that will be consider one "sequence"
+        self.time_window_size = time_window_size
+
+        # current index in iteration
+        self._curr_idx = 0
+
+        # load pt as x,y and apply transformations
+        self.X, self.y = transform(None), target_transform(None)
+
+        # make sure we have labels for all data
+        assert len(self.X)==len(self.y)
+
+    def __iter__(self):
+        while self._curr_idx < (len(self.X) - self.time_window_size + 1):
+            # return current time window chunk and corresponding ground truth
+            yield self.X[self._curr_idx: self._curr_idx + self.time_window_size, ...], \
+                    self.y[self._curr_idx: self._curr_idx + self.time_window_size, ...]
+
+            # increment for next
+            self._curr_idx += 1
+
+    def __len__(self):
+        return len(self.X) - self.time_window_size + 1
+
+
 if __name__ == "__main__":
-    # TRAIN_DIR = '/home/kachauha/Downloads/data_Q4_2018_serials'
-    train_serials = ['ZA13Q5GK']
-    DATA_ROOT_DIR = '/home/kachauha/Downloads/data_Q4_2018_serials/failed'
-    use_cols = ['smart_1_raw', 'smart_5_raw', 'status']
+    # ##################################### CONVERT CSV TO PT ##################################### #
+    # data location
+    ROOT_DIR = '/home/kachauha/Downloads/data_Q4_2018_serials'
+    META_DIR = ospj(ROOT_DIR, 'meta')
 
-    # create by chaining single serial datsets
-    train_dataset = torch.utils.data.ChainDataset(
-        BackblazeSingleDriveDataset(os.path.join(DATA_ROOT_DIR, serial + '.csv'), feat_cols=use_cols)
-        for serial in train_serials)
+    # same name as original root dir but suffixed with _pt
+    SAVE_ROOT_DIR = ROOT_DIR + '_pt'
 
-    # train_dataset = BackblazeSingleDriveDataset(fpath=drive_data_path)
-    # train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=1, shuffle=True)
-    for seq,label in train_dataset:
-        print('Label = ', label, '\nData = ')
-        print(seq)
+    # get data from all dirs
+    all_ser_files = [f for f in gg(ospj(ROOT_DIR, '**/*.csv')) if os.path.isfile(f)]
+
+    # columns used as features and target
+    target_cols = ['status']
+    feat_cols = list(pd.read_csv(ospj(META_DIR, 'means.csv'), header=None)[0])
+
+    # for ser_fpath in all_ser_files:
+    def save_ser(ser_fpath):
+        # decompose file path
+        fparts = ser_fpath.split('/')
+        subfolder = fparts[-2]
+        ser = fparts[-1].split('.')[0]
+
+        # load dataframe
+        serdf = pd.read_csv(ser_fpath, usecols = ['date'] + feat_cols + target_cols)
+        serdf = serdf.sort_values(by='date', ascending=True)
+        serdf = serdf.drop('date', axis=1)
+
+        # convert to tensor and save
+        torch.save(obj=(serdf[feat_cols].values, serdf[target_cols].values), \
+                    f=ospj(SAVE_ROOT_DIR, subfolder, ser+'.pt'))
+
+    _ = Parallel(n_jobs=-1, prefer='threads')(delayed(save_ser)(ser_fpath) for ser_fpath in all_ser_files)
