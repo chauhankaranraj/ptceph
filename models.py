1856841e70f423081d70fa4e073e819a47f53b87
Karanraj Chauhan <chauhank@bu.edu>
2019-10-31 10:07:42 -0400

    Added experimental lstm archtecture

--- models.py
+++ models.py
@@ -19,3 +19,28 @@ class LSTMtoy(nn.Module):
         logits = self.linear(output[-1, :])
         return logits
 
+
+class LSTMExp(nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super(LSTMExp, self).__init__()
+        self.lstm_layers = [
+            nn.LSTM(input_dim, 256, 2),
+            nn.LSTM(256, 512, 2),
+            nn.LSTM(512, 1024, 1),
+            nn.LSTM(1024, 512, 2),
+            nn.LSTM(512, 256, 2)
+        ]
+        self.linear = nn.Linear(self.lstm_layers[-1].hidden_size, output_dim)
+
+    def forward(self, x):
+        # pass input to first lstm layers obj
+        output, _ = self.lstm_layers[0](x)
+
+        # pass output of previous lstm layers to all of the remaining ones
+        for lstm in self.lstm_layers[1:]:
+            output, _ = lstm(output)
+
+        # feed output of last lstm cell for the last time cell to linear layer
+        logits = self.linear(output[-1, :])
+        return logits
+
